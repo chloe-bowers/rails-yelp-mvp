2cde00ba52a7a754e2dec940f5c2ecfdb33f231f
Chloe Bowers <chlo_bo@live.com.au>
2022-11-16 16:21:28 +0100

    finsished

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,50 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[ show ]
+
+  # GET /restaurants
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET /restaurants/1
+  def show
+  end
+
+  # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # POST /restaurants
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: "Restaurant was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /restaurants/1
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant, notice: "Restaurant was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /restaurants/1
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    resources :reviews, only: [ :new, :create ]
+
+    collection do
+      # get :top
+      get "top", to: "restaurants#top", as: "paul"
+    end
+
+    member do
+      get :chef
+    end
+  end
+
+  resources :reviews, only: [ :destroy ]
 end
